026d83acb6c5d69583671be6e87539831a311400
Dae Hyeon Kim <dhkim1016@kw.ac.kr>
2023-12-20 17:21:07 +0900

    Refactoring

--- Learn/Criterion.py
+++
@@ -1,4 +0,0 @@
-import torch.nn as nn
-
-def criterion(out, label):
-    return nn.functional.cross_entropy(out, label)
--- data/Verification/__init__.py
+++
@@ -1,5 +0,0 @@
-from Neighbors import neighbors_verification
-
-__all__ = [
-    'neighbors_verification'
-]
